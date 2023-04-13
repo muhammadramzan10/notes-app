1f2d0a3d2c02192371bae0b866547adebb69c227
M Ramzan Magsi <59991186+muhammadramzan10@users.noreply.github.com>
2023-04-13 02:42:46 -0700

    checking

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -4,7 +4,8 @@ export function NewNote(){
     return (
         <>
             <h1 className='mb-4'>New Note</h1>
-            <NoteForm />    
+            <NoteForm />
+            checking for changes
         </>
     )
 }
